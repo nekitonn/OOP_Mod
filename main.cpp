3c44f87f891777170029ad2a1ef68f54cd17ddd1
“nekitonn” <nekitonn@gmail.com>
2021-05-14 15:48:25 +0300

    -

--- main.cpp
+++ main.cpp
@@ -6,11 +6,14 @@ int main() {
     string Input = "ab ra ka d ab r a";
     int count_Letter_a(string Input);
     int count_a = 0;
-    for (int i = 0; Input[i] != '\0'; ++i)
-        if (Input[i] == 'a') count_a++;
-    size_t count_Letter_ab = count(Input.begin(), Input.end(), 'ab');
+    int count_ab = 0
+    for (int i = 0; Input[i] != '\0'; i++)
+        if
+        (Input[i] == 'a') count_a++;
+            if (Input[i+1] != 'b') count_ab++;
 
-    cout << count_Letter_ab;
+
+    cout <<count_ab;
     cout <<count_a;
     return 0;
 }

77f5efb4f7ed1bde91669638e3d9d0b9bb7f3d08
“nekitonn” <nekitonn@gmail.com>
2021-05-14 16:00:05 +0300

    -

--- main.cpp
+++ main.cpp
@@ -3,17 +3,19 @@
 using namespace std;
 
 int main() {
-    string Input = "ab ra ka d ab r a";
+    string Input = "abrakadabra";
     int count_Letter_a(string Input);
     int count_a = 0;
-    int count_ab = 0
+    int count_ab = 0;
     for (int i = 0; Input[i] != '\0'; i++)
-        if
-        (Input[i] == 'a') count_a++;
-            if (Input[i+1] != 'b') count_ab++;
+        if (Input[i] == 'a'){
+            if (Input[i + 1] != 'b')
+                count_ab++;
+        } else count_a++;
 
 
-    cout <<count_ab;
+
+    cout <<count_ab << " ";
     cout <<count_a;
     return 0;
 }

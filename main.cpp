e36cdf6eb8936bdc0e27b1a941069a5627dc3f16
nekitonn <84184224+nekitonn@users.noreply.github.com>
2021-05-14 15:14:05 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string Input = "abrakadabra";
+    int count_Letter_a(string Input);
+    int count_a = 0;
+
+    for (int i = 0; i < Input.size(); i++)
+        if (Input[i] == 'a') count_a++;
+
+    int count_Letter_ab(string Input);
+    int count_ab = 0;
     return 0;
 }
